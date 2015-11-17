e0d7766cd8440d44f64e1f481bbb1fd3247e519a
Daniel Suo <danielsuo@gmail.com>
2015-11-17 15:43:06 -0500

    Delete Makefile; convert to cmake

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -22,22 +22,25 @@ int main(int argc, char *argv[]) {
   FILE *fp;
   Data data;
 
-  fp = fopen("gold", "r+");
-  data.digest(fileno(fp));
-  fp = fopen("silver", "r+");
-  data.digest(fileno(fp));
-  fp = fopen("blue", "r+");
-  data.digest(fileno(fp));
-  fp = fopen("pink", "r+");
+  // fp = fopen("gold", "r+");
+  // data.digest(fileno(fp));
+  // fp = fopen("silver", "r+");
+  // data.digest(fileno(fp));
+  // fp = fopen("blue", "r+");
+  // data.digest(fileno(fp));
+  // fp = fopen("pink", "r+");
+  // data.digest(fileno(fp));
+
+  fp = fopen("iPhone", "r+");
   data.digest(fileno(fp));
 #else
   TCPServer *server = new TCPServer(8124);
 
-  server->add_device(new Device((char *)"gold", (char *)"192.168.0.105", 8124));
-  server->add_device(new Device((char *)"silver", (char *)"192.168.0.106", 8124));
-  server->add_device(new Device((char *)"blue", (char *)"192.168.0.107", 8124));
-  server->add_device(new Device((char *)"pink", (char *)"192.168.0.108", 8124));
-  // server->add_device(new Device((char *)"iPhone", (char *)"192.168.0.109", 8124));
+  // server->add_device(new Device((char *)"gold", (char *)"192.168.0.105", 8124));
+  // server->add_device(new Device((char *)"silver", (char *)"192.168.0.106", 8124));
+  // server->add_device(new Device((char *)"blue", (char *)"192.168.0.107", 8124));
+  // server->add_device(new Device((char *)"pink", (char *)"192.168.0.108", 8124));
+  server->add_device(new Device((char *)"iPhone", (char *)"192.168.0.109", 8124));
 
   server->listen();
   server->connect();
